44f2b00554bc859b28648944b175193c31fcbe05
AGL_Hồ Sỹ Huy  (フイ) <ho_sy_huy@allgrow-labo.jp>
2022-12-07 09:53:16 +0700

    mainvisual

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,1 +1,12 @@
-console.log("main.js");
+$(document).ready(function () {
+    $(".c-mainvisual__slider").slick({
+      fade: true,
+      speed: 1100,
+      infinite: true,
+      autoplay: true,
+      cssEase: "linear",
+      arrows: false,
+      dots: true,
+    });
+  });
+  
