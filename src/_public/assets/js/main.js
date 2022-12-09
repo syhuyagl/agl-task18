1ff8651bbc71e45991a462b8c492b4210c9ae27a
AGL_Hồ Sỹ Huy  (フイ) <ho_sy_huy@allgrow-labo.jp>
2022-12-09 17:19:39 +0700

    almost

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -8,5 +8,22 @@ $(document).ready(function () {
       arrows: false,
       dots: true,
     });
+    
+    $('.c-top').mouseenter(function(){
+      $('.c-top__button1').removeClass('is-show')
+      $('.c-top__button2').addClass('is-show')
+    })
+    $('.c-top').mouseleave(function(){
+      $('.c-top__button1').addClass('is-show')
+      $('.c-top__button2').removeClass('is-show')
+    })
+    $(".c-top").click(function () {
+      console.log("#1312");
+      $("html, body").animate(
+        {
+          scrollTop: 0,
+        },
+        1000
+      );
+    })
   });
-  
